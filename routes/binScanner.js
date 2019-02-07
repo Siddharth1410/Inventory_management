bba34d2f1c9f6c5f1ca2a866d0c091b843a379d7
Dannelor <masonbeckham@yahoo.com>
2019-02-06 21:55:38 -0600

    Clean up generated code and change public directory names

--- routes/binScanner.js
+++ routes/binScanner.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', (req,res) => {
-    res.render('binScanner',{})
+router.get('/', (req, res) => {
+  res.render('binScanner', {})
 })
 
 module.exports = router
